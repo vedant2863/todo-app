8766c8aa0d254cf26ccc90b2e8fad255d7e30618
vedant2863 <jvedant727@gmail.com>
2024-06-10 12:36:29 +0530

    config file setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,62 +1,14 @@
-import { z } from "zod";
-import { Hono } from "hono";
-import { logger } from "hono/logger";
-import { poweredBy } from "hono/powered-by";
-import { prettyJSON } from "hono/pretty-json";
-
-import authRoutes from "./routes/auth.routes";
-import todoRoutes from "./routes/todo.routes";
+import { config } from "./utils/config";
 import connectDB from "./utils/connectDB";
 
-// app
-const app = new Hono()
-  .basePath("/api")
-  .route("/auth", authRoutes)
-  .route("/todos", todoRoutes);
 
 
-
-// middleware
-app.use("*", logger(), prettyJSON(), poweredBy());
-
-// Custom Not Found Message
-app.notFound((c) => {
-  return c.text("Custom 404 Not Found", 404);
-});
-
-// Error handling
-app.onError((err, c) => {
-  console.error(`${err}`);
-  return c.text("Custom Error Message 500", 500);
-});
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
-
-//index
-const ServeEnv = z.object({
-  PORT: z
-    .string()
-    .regex(/^\d+$/, "Port must be a numeric string")
-    .default("3000")
-    .transform(Number),
-});
-const ProcessEnv = ServeEnv.parse(process.env);
-
-const server = Bun.serve({
-  port: ProcessEnv.PORT,
-  hostname: "0.0.0.0",
-  fetch: app.fetch,
-});
-
-// console.log(server);
 
 
 
 connectDB()
   .then(() => {
-    console.log("⚙️ Server is running at port : ", server.port);
+    console.log("⚙️ Server is running at port : ", config.PORT);
   })
   .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
---
+++ server/src/utils/config.ts
@@ -0,0 +1,9 @@
+
+
+const _config = {
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    PORT: process.env.PORT,
+}
+
+export const config = Object.freeze(_config);
